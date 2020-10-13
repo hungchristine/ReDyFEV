53105382fd735672f5a5b7051912d5a3609c3ae5
Christine Hung <christine.hung@ntnu.no>
2020-10-13 12:47:15 +0200

    Refactor code to allow running of experiments
    Allow users to define sampling period for electricity data, BEV and ICEV lifetimes, electricity trade assumption
    (flow tracing or Moro and Lonza), battery manufacturing country el CO2 intensity

--- entso_data_clean.py
+++ entso_data_clean.py
@@ -5,53 +5,57 @@
 """
 import pandas as pd
 import numpy as np
+from datetime import datetime, date, time, timezone
 import pickle
 import os
+import logging
 
-fp = os.path.abspath(os.path.curdir)
-fp_output = os.path.join(os.path.curdir, 'output')
-os.chdir(fp_output)
-
-## Load previous results
-with open(r'entso_export_trade.pkl', 'rb') as handle:
-    trade_dict = pickle.load(handle)
-with open(r'entso_export_gen.pkl', 'rb') as handle:
-    gen_dict = pickle.load(handle)
-    
 #%%
 def checkEqual(iterator):
     return len(set(iterator)) <= 1
 
-def aggregate_entso(dictionary):    
+def aggregate_entso(dictionary, start=None, end=None):
     new_dict = {}
     summed_dict = {}
     for key, value in dictionary.items():
         timestep_list = []
+        if start is not None and end is not None:
+            try:
+                value = value.loc[start:end]
+                print(value)
+            except AttributeError:
+                print('No dataframe, cannot take slice!')
+
         try:
-            if isinstance(value, pd.DataFrame) or isinstance(value,pd.Series):
-                for i in range(0,value.shape[0]-1):
+            if isinstance(value, pd.DataFrame) or isinstance(value, pd.Series):
+                for i in range(0, value.shape[0]-1):
                     try:
-                        timestep = (value.index[i+1] - value.index[i]).total_seconds()/3600 # get timestep in hours
+                        timestep = (value.index[i+1] - value.index[i]).total_seconds() / 3600  # get timestep in hours
                         timestep_list.append(timestep)
                     except IndexError:
                         print(f'IndexError {i}')
                     except:
                         print('Could not perform!')
-                        
+
+        # TODO: deal with negative values in hydropower pumped storage... perhaps not necessary, given the annual aggregation? maybe just add a check...
+#        if production['Hydropower Pumped Storage'] < 0:
+            # remove proportional amounts of all other
+
+
                 # Make sure timesteps are all equal length before calculating
                 # NB: this is made obsolete by using bentso's fullyear=True
                 if checkEqual(timestep_list):
-                    summed_dict[key] = (value*timestep_list[0]/1e6).sum() # To calculate electricity generated; take power per timestep and multiply by length of time period. Sum over whole year
+                    summed_dict[key] = (value * timestep_list[0] / 1e6).sum() # To calculate electricity generated; take power per timestep and multiply by length of time period. Sum over whole year
                     new_dict[key] = value
                 else:
                     print(f'warning: unequal time steps for {key}')
-                    if min(timestep_list)==1:
+                    if min(timestep_list) == 1:
                         new_dict[key] = value.resample('1H').interpolate()
-                        summed_dict[key] = (new_dict[key]*timestep_list[0]/1e6).sum()
+                        summed_dict[key] = (new_dict[key] * timestep_list[0] / 1e6).sum()
                         print('1 hour')
-                    elif min(timestep_list)==0.25:
+                    elif min(timestep_list) == 0.25:
                         new_dict[key] = value.resample('15T').interpolate()
-                        summed_dict[key] = (new_dict[key]*timestep_list[0]/1e6).sum()
+                        summed_dict[key] = (new_dict[key] * timestep_list[0] / 1e6).sum()
                         print('15 minutes')
                     else:
                         print('very uneven timesteps')
@@ -60,57 +64,72 @@ def aggregate_entso(dictionary):
            print(f'something went wrong in {key}')
     return summed_dict, new_dict
 
+
 def build_trade_mat(trade_dict):
     mi = trade_dict.keys()
     trade_mat = pd.DataFrame(trade_dict.values(), index=mi)
     trade_mat = trade_mat.unstack()
-    trade_mat.columns = trade_mat.columns.get_level_values(1) # remove extraneous level from column labels
+    trade_mat.columns = trade_mat.columns.get_level_values(1)  # remove extraneous level from column labels
     return trade_mat
 
 
  #%%
-generation = gen_dict.copy()
-trade = trade_dict.copy()
+def clean_entso(start=None, end=None):
+    fp = os.path.abspath(os.path.curdir)
+    fp_output = os.path.join(os.path.curdir, 'output')
+    os.chdir(fp_output)
 
-summed_gen, new_gen = aggregate_entso(generation)
-summed_trade, new_trade = aggregate_entso(trade)
+    ## Load previous results
+    with open(r'entso_export_trade.pkl', 'rb') as handle:
+        trade_dict = pickle.load(handle)
+    with open(r'entso_export_gen.pkl', 'rb') as handle:
+        gen_dict = pickle.load(handle)
 
-trade_df = build_trade_mat(summed_trade)
-gen_df = pd.DataFrame.from_dict(summed_gen, orient='index')
+    generation = gen_dict.copy()
+    trade = trade_dict.copy()
 
-""" Read in Ireland's data manually from .csv export from Transparency Platform; 
-    'country' (which Bentso fetches) and the 'bidding zone' classifications differ,
-    for which the latter has higher production values (which agree better with statistical 
-    production from 2018)
-"""
+#    start = datetime(2019, 1, 1, 0)  # test values for sub-annual periods
+#    end = datetime(2019, 6, 30, 23)
+    summed_gen, new_gen = aggregate_entso(generation, start, end)
+    summed_trade, new_trade = aggregate_entso(trade, start, end)
 
-fp_ie = os.path.join(fp, 'data','gen_IE.csv')
-new_ie = pd.read_csv(fp_ie)
-new_ie = new_ie.set_index('MTU', drop=True, append=False).drop('Area', axis=1)
-new_ie = new_ie.replace('n/e', np.nan)
-new_ie = (new_ie*0.5).sum()/1e6 # samples on the half hour
-new_ie = new_ie.drop(index='Marine  - Actual Aggregated [MW]')
-new_ie.index = gen_df.columns
+    trade_df = build_trade_mat(summed_trade)
+    gen_df = pd.DataFrame.from_dict(summed_gen, orient='index')
 
-gen_df.loc['IE'] = new_ie
+    """ Read in Ireland's data manually from .csv export from Transparency Platform;
+        'country' (which Bentso fetches) and the 'bidding zone' classifications differ,
+        for which the latter has higher production values (which agree better with statistical
+        production from 2018)
+    """
 
-""" Aggregate GB and GB_NIR regions """
-gen_df = (gen_df.reset_index().replace({'index':{'GB-NIR':'GB'}}).groupby('index',sort=False).sum())
-trade_df = (trade_df.reset_index().replace({'index':{'GB-NIR':'GB'}}).groupby('index',sort=False).sum()) # aggregate trade rows
-trade_df = ((trade_df.T).reset_index().replace({'index':{'GB-NIR':'GB'}}).groupby('index',sort=False).sum()).T # aggregate trade column
+    fp_ie = os.path.join(fp, 'data', 'gen_IE.csv')
+    new_ie = pd.read_csv(fp_ie)
+    new_ie = new_ie.set_index('MTU', drop=True, append=False).drop('Area', axis=1)
+    new_ie = new_ie.replace('n/e', np.nan)
+    new_ie = (new_ie * 0.5).sum() / 1e6  # samples on the half hour
+    new_ie = new_ie.drop(index='Marine  - Actual Aggregated [MW]')
+    new_ie.index = gen_df.columns
 
-""" Add Cyprus to trade df (no trade relationships) """
-trade_df['CY'] = 0 
-trade_df.loc['CY'] = 0
+    gen_df.loc['IE'] = new_ie
 
-# Add in countries with trade relationships, but no generation data; assume 0 production
-add_countries = list(set(trade_df.index)-set(gen_df.index))
-for country in add_countries:
-    gen_df.loc[country] = 0
+    """ Aggregate GB and GB_NIR regions """
+    gen_df = (gen_df.reset_index().replace({'index': {'GB-NIR':'GB'}}).groupby('index', sort=False).sum())
+    trade_df = (trade_df.reset_index().replace({'index': {'GB-NIR':'GB'}}).groupby('index', sort=False).sum())  # aggregate trade rows
+    trade_df = ((trade_df.T).reset_index().replace({'index': {'GB-NIR':'GB'}}).groupby('index', sort=False).sum()).T  # aggregate trade column
 
-with open(r'trade_final.pkl', 'wb') as handle:
-    pickle.dump(trade_df, handle)
-with open(r'gen_final.pkl', 'wb') as handle:
-    pickle.dump(gen_df, handle)
-    
-os.chdir(fp)
+    """ Add Cyprus to trade df (no trade relationships) """
+    trade_df['CY'] = 0
+    trade_df.loc['CY'] = 0
+
+    # Add in countries with trade relationships, but no generation data; assume 0 production
+    add_countries = list(set(trade_df.index) - set(gen_df.index))
+    for country in add_countries:
+        gen_df.loc[country] = 0
+
+    with open(r'trade_final.pkl', 'wb') as handle:
+        pickle.dump(trade_df, handle)
+    with open(r'gen_final.pkl', 'wb') as handle:
+        pickle.dump(gen_df, handle)
+
+    logging.info('Completed export of ENTSO-E data')
+    os.chdir(fp)
--- main.py
+++ main.py
@@ -3,29 +3,62 @@
 This script runs the data extraction, data cleaning, calculations and visualization
 scripts used in "Regionalized footprints of battery electric vehicles in Europe"
 
+Users can run two types of experiments concurrently; the electricity sampling period
+('el_experiments'), and vehicle parameters ('BEV_experiments')
 """
+import logging
+from datetime import datetime
+
+logname = 'run ' + datetime.now().strftime('%d-%m-%y, %H-%M')
+logging.basicConfig(filename=logname,
+                            filemode='a',
+                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%H:%M:%S',
+                            level=logging.DEBUG)
 
 if __name__ == "__main__":
-    #import bentso_extract
-    # import entso_data_clean
-    # import hybridized_impact_factors
+    import bentso_extract
+    import entso_data_clean
+    import hybridized_impact_factors as hybrid
     import BEV_footprints_calculations as calc
     import BEV_visualization as viz
 
-    experiments = {'baseline': [180000, 200000, True],
-                   # 'no_LEONTIEF': [180000, 20000, False]
-                   'long_BEV_life': [200000, 200000, True],
-                   'short_BEV_life': [160000, 200000, True]}
+    el_experiments = {'baseline': [2019]}
+    # TODO: combine bentso_extract and entso_data_clean
+    for el_exp, params in el_experiments.items():
+        logging.info(f'Starting el_experiment {el_exp}')
+        bentso_extract.bentso_query(params[0])  # can specify year of analysis here
+        if len(params) > 1:
+            logging.info(f'year: {params[0]}')
+            logging.info(f'subperiod start: {params[1]}')
+            logging.info(f'subperiod end: {params[2]}')
+            entso_data_clean.clean_entso(params[1], params[2]) # can specify start and end of sub-annual analysis period here as datetime object
+        else:
+            logging.info(f'year: {params[0]}')
+            entso_data_clean.clean_entso()
+        logging.info('Starting calculation of hybridized emission factors')
+        hybrid.hybrid_emission_factors()
+        hybrid.clean_impact_factors()
 
-    for exp, params in experiments.items():
-        print('Performing experiment ' + exp)
-        calc.run_stuff(run_id=exp,
-                       BEV_lifetime=params[0],
-                       ICEV_lifetime=params[1],
-                       leontief_el=True,  # params[2]
-                       production_el_intensity=684,
-                       export_data=True,
-                       include_TD_losses=True
-                       )
-        viz.visualize(exp, export_figures=True, include_TD_losses=True)
 
+        BEV_experiments = {
+                       'baseline': [180000, 180000, False]#,
+                       'flow_tracing': [180000, 200000, True],
+                       'long_BEV_life': [200000, 180000, False],
+                       'short_BEV_life': [160000, 180000, False]
+                      }
+
+        for BEV_exp, params in BEV_experiments.items():
+            print('Performing experiment ' + BEV_exp)
+            logging.info(f'Starting BEV experiment {BEV_exp}')
+            calc.run_stuff(run_id=BEV_exp,
+                           BEV_lifetime=params[0],
+                           ICEV_lifetime=params[1],
+                           leontief_el=params[2],
+                           production_el_intensity=684,
+                           export_data=True,
+                           include_TD_losses=True
+                           )
+            logging.info('Visualizing results...')
+            viz.visualize(BEV_exp, export_figures=True, include_TD_losses=True)
+            logging.info(f'Completed experiment electricity {el_exp}, BEV scenario {BEV_exp}')
